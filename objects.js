abc5904e7aa7a275162881a5eb7c20fd2a675183
Skin <rizqinnazhif@gmail.com>
2019-11-24 10:34:52 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+  skapara: "Suikinkutsu",
+  gramophoneAllStar: "I Don't Know",
+  slipknot: "A Liar's Funeral"
+}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist['artistName'];
+  // return playlist;
+}
